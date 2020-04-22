3c999f0f7615a104d5b2e2522fc61a2ae27e3b7f
Andrey Matin <creohan@gmail.com>
2020-04-23 01:12:23 +0300

    Added test page Removed unused styles Clean dublicates Added automation by gulp

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # SCSS Reset
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
 Reset compilation = modern reset + meyer's reset + normalizer
 
 ## Features
 
 - Best practices from https://github.com/hankchizljaw/modern-css-reset
-- Modernized reset from https://meyerweb.com/eric/tools/css/reset/
+- Modernized styles reset of https://meyerweb.com/eric/tools/css/reset/
 - Reset styles only from https://necolas.github.io/normalize.css/
 - Typography defaults
 - Predefined variables
---
+++ gulpfile.js
@@ -0,0 +1,152 @@
+(() => {
+
+  'use strict';
+
+  const {
+    src,
+    dest,
+    parallel,
+    series,
+    watch
+  } = require('gulp');
+
+  /**
+   * Modules
+   */
+
+  // Styles
+  const sass = require('gulp-sass');
+  const postcss = require('gulp-postcss');
+  const autoprefixer = require('autoprefixer');
+  const cssnano = require('cssnano');
+  // Server
+  const connect = require('gulp-connect');
+  const open = require('gulp-open');
+
+  // Notification
+  const plumber = require('gulp-plumber');
+  const notify = require('gulp-notify');
+
+  // Source Maps
+  const sourcemaps = require('gulp-sourcemaps');
+
+  /**
+   * Config
+   */
+  const cfg = {
+    src: {
+      scss: './src/scss/**/*.scss',
+      css: './src/css/**/*.css',
+      html: './test/*.html'
+    },
+    server: {
+      host: '0.0.0.0',
+      root: './test/',
+      port: 5500,
+      src: './test/index.html',
+      uri: 'http://localhost:5500/'
+    },
+    dest: {
+      scss: './src/scss/',
+      css: './src/css/',
+      cssDist: './dist/css/',
+      cssTest: './test/css/'
+    },
+  };
+
+
+  /**
+   * Styles
+   */
+  const styles = () =>
+    src(cfg.src.scss)
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      //outputStyle: 'compressed',
+      errLogToConsole: false
+    }))
+    .on('error', notify.onError())
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(cfg.dest.css))
+    .pipe(connect.reload());
+
+
+
+  /**
+   * PostCSS, Autoprefixer
+   */
+  const css = () =>
+    src(cfg.dest.css + '/**/*.css')
+    .pipe(plumber())
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano()
+    ]))
+    .on('error', notify.onError())
+    .pipe(dest(cfg.dest.cssDist))
+    .pipe(connect.reload());
+
+
+  /**
+   * HTML
+   */
+  const html = () =>
+    src(cfg.src.html)
+    .pipe(connect.reload())
+
+
+
+
+  /**
+   * Server
+   */
+
+
+  /**
+   * Create Local Web Server
+   */
+  const openServer = () => {
+    connect.server({
+      host: cfg.server.host,
+      root: cfg.server.root,
+      port: cfg.server.port,
+      livereload: true
+    });
+  };
+
+  /**
+   * Open Default Browser
+   */
+  const openBrowser = () =>
+    src(cfg.server.src)
+    .pipe(plumber())
+    .pipe(open({
+      uri: cfg.server.uri
+    }));
+
+  /**
+   * Watcher
+   */
+  const watcher = () => {
+    watch(cfg.src.scss, series(styles, css, copyCSS));
+    watch(cfg.src.html, html);
+  };
+
+
+  /**
+   * Copy Test Styles
+   */
+
+  const copyCSS = () =>
+    src(cfg.dest.cssDist + '*')
+    .pipe(dest(cfg.dest.cssTest));
+
+  /**
+   * Tasks
+   */
+
+  // Development Tasks
+  exports.default = parallel(series(styles, css, copyCSS), openServer, openBrowser, watcher);
+})();
